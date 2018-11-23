a8e6c57d9b819d0cd344f6043994afc1f7a8dc86
visrahane <rahane@usc.edu>
2018-11-22 21:52:37 -0800

    create atom distribution array for spatial decomposition

--- sequential_monte_carlo.c
+++ sequential_monte_carlo.c
@@ -2,112 +2,54 @@
 #include "mpi.h"
 #include<stdio.h>
 #include <stdlib.h>
-#include<math.h>
+#include <math.h>
 
-#define CUT_OFF 3.25
-#define X 64
-#define Y 64
-#define Z 64
+#define CUT_OFF 1.5
+#define X 1060.28
+#define Y 212.06
+#define Z 212.06
 #define LEN(x)  (sizeof(x) / sizeof((x)[0]))
+long totalPoints = 0;
 
 struct AtomData {
 	int noOfAtoms;
-	float* atomCoords[4];
-}AtomDataObj;
-//typedef struct AtomData AtomData;
+	float* atomCoords[3];
+}AtomDataObj,ProcAtomObj;
 
-/*long getHits(int noOfRandomPts) {
-	int hitsCount = 0;
-	float x, y, z;
-	FILE *fop = fopen("mc.out", "w");
-	srand((unsigned int)time(NULL));
-	for (int i = 0; i < noOfRandomPts; i++) {
-		//generate point
-		x = ((double)rand() / (double)(RAND_MAX)) * X;
-		y = ((double)rand() / (double)(RAND_MAX)) * Y;
-		z = ((double)rand() / (double)(RAND_MAX)) * Z;
-
-		//check if within sys or outside
-		for (int j = 0; j < AtomDataObj.noOfAtoms; j++) {
-			float xa = AtomDataObj.atomCoords[0][j];
-			float ya = AtomDataObj.atomCoords[1][j];
-			float za = AtomDataObj.atomCoords[2][j];
-			if ((abs(x - xa) <= CUT_OFF) && (abs(y - ya) <= CUT_OFF) && (abs(z - za) <= CUT_OFF)) {
-				printf("\natom coords %f %f %f", xa, ya, za);
-				printf("\nrandom Pt %f %f %f", x, y, z);
-				hitsCount++;
-				break;
-			}
-		}
-
-	}
-	return hitsCount;
-
-}*/
-
-/*long getHitsBySequentialPoints() {
-	int hitsCount = 0;
-	float xyz[3];
-	FILE *fop = fopen("mc.out", "w");
-	//to start with different seed for random generator
-	//srand((unsigned int)time(NULL));
-	//generate point
-	int x,y,z,i;
-	for (x = 0; x < X;) {
-		for ( y = 0; y < Y;) {
-			for ( z = 0; z < Z;) {
-				//check if within sys or outside
-				for ( i = 0; i < AtomDataObj.noOfAtoms; i++) {
-					float xa = AtomDataObj.atomCoords[0][i];
-					float ya = AtomDataObj.atomCoords[1][i];
-					float za = AtomDataObj.atomCoords[2][i];
-					if ((abs(x - xa) <= CUT_OFF) && (abs(y - ya) <= CUT_OFF) && (abs(z - za) <= CUT_OFF)) {
-						//fprintf(fop, "\natom coords %f %f %f", xa, ya, za);
-						//fprintf(fop, "\nrandom Pt %f %f %f", xyz[0], xyz[1], xyz[2]);
-						printf("\natom coords %f %f %f", xa, ya, za);
-
-						hitsCount++;
-						break;
-					}
-				}
-				z += CUT_OFF;
-
-			}
-			y += CUT_OFF;
-		}
-		x += CUT_OFF;
-
-	}
-	return hitsCount;
-
-}*/
 long getHitsByPSequentialPoints(int procNo) {
 	int hitsCount = 0;
 	float xyz[3];
-	FILE *fop = fopen("mc.out", "w");
+	//	FILE *fop = fopen("mc.out", "w");
 	//
-	int procs[3] = { 2,2,2 };
+	int procs[3] = { 20,2,2 };
 	int startX = procNo / (procs[1] * procs[2]);
-	startX *= X / procs[0];
+	float XPerProc = X / procs[0];
+	startX *= XPerProc;
 
 	int startY = (procNo / procs[2]) % procs[1];
-	startY *= Y / procs[1];
-	int startZ = procNo % procs[2];
-	startZ *= Z / procs[2];
-	//	printf("\n%d,%d,%d", startX, startY, startZ);
+	float YPerProc = Y / procs[1];
+	startY *= YPerProc;
 
-		//generate point
+	int startZ = procNo % procs[2];
+	float ZPerProc = Z / procs[2];
+	startZ *= ZPerProc;
+
+	int countOfAtoms=fetchLocalAtoms(procs, procNo);
+	//generate point
 	float x, y, z;
-	int i;
-	for (x = startX; x < startX + X / procs[0];) {
-		for (y = startY; y < startY + Y / procs[1];) {
-			for (z = startZ; z < startZ + Z / procs[2];) {
+	int i, points = 0;
+	double t1 = MPI_Wtime();
+		
+	for (x = startX; x < startX + XPerProc; x += CUT_OFF) {
+		for (y = startY; y < startY + YPerProc; y += CUT_OFF) {
+			for (z = startZ; z < startZ + ZPerProc; z += CUT_OFF) {
 				//check if within sys or outside
-				for (i = 0; i < AtomDataObj.noOfAtoms; i++) {
-					float xa = AtomDataObj.atomCoords[0][i];
-					float ya = AtomDataObj.atomCoords[1][i];
-					float za = AtomDataObj.atomCoords[2][i];
-					if ((abs(x - xa) <= CUT_OFF) && (abs(y - ya) <= CUT_OFF) && (abs(z - za) <= CUT_OFF)) {
+				points++;
+				for (i = 0; i < countOfAtoms; i++) {
+					float xa = ProcAtomObj.atomCoords[0][i];
+					float ya = ProcAtomObj.atomCoords[1][i];
+					float za = ProcAtomObj.atomCoords[2][i];
+					if ((fabs(x - xa) <= CUT_OFF) && (fabs(y - ya) <= CUT_OFF) && (fabs(z - za) <= CUT_OFF)) {
 						//fprintf(fop, "\natom coords %f %f %f", xa, ya, za);
 						//fprintf(fop, "\nrandom Pt %f %f %f", xyz[0], xyz[1], xyz[2]);
 						//printf("\natom coords %f %f %f", xa, ya, za);
@@ -116,79 +58,84 @@ long getHitsByPSequentialPoints(int procNo) {
 						break;
 					}
 				}
-				z += CUT_OFF;
-
 			}
-			y += CUT_OFF;
 		}
-		x += CUT_OFF;
-
 	}
+	printf("time for for loop is %f\n", MPI_Wtime() - t1);
+	printf("points:%d\n", points);
+	MPI_Allreduce(&points, &totalPoints, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 	return hitsCount;
 
 }
 
+int fetchLocalAtoms(int procs[],int myid) {
+	int i, count=0,xn,yn,zn,sid;
+	float XPerProc = X / procs[0];
+	float YPerProc = Y / procs[1];
+	float ZPerProc = Z / procs[2];
+	for (i = 0; i < 3; i++)
+	{
+		ProcAtomObj.atomCoords[i] = (float*)malloc(AtomDataObj.noOfAtoms * sizeof(float));
+	}
 
-void readAtoms() {
+	for (i = 0; i < AtomDataObj.noOfAtoms; i++)
+	{
+		xn = (int)(AtomDataObj.atomCoords[0][i] / XPerProc);
+		yn = (int)(AtomDataObj.atomCoords[1][i] / YPerProc);
+		zn = (int)(AtomDataObj.atomCoords[2][i] / ZPerProc);
+		sid = xn * procs[1] * procs[2] + yn * procs[2] + zn;
+		if (sid == myid)
+		{
+			ProcAtomObj.atomCoords[0][count] = AtomDataObj.atomCoords[0][i];
+			ProcAtomObj.atomCoords[1][count] = AtomDataObj.atomCoords[1][i];
+			ProcAtomObj.atomCoords[2][count] = AtomDataObj.atomCoords[2][i];
+			count++;
+		}
+	}
+	return count;
+}
+void readAtoms(char *file_name) {
 	/* Open an MD-configuration file */
-	FILE *fp = fopen("atom_coords.in", "r");
+	FILE *fp = fopen(file_name, "r");
 	int i;
 	/* Read the # of atoms */
 	fscanf(fp, "%d", &AtomDataObj.noOfAtoms);
-	for (i = 0; i < 4; i++) {
+	for (i = 0; i < 3; i++) {
 		AtomDataObj.atomCoords[i] = (float*)malloc(AtomDataObj.noOfAtoms * sizeof(float));
 	}
 	for (i = 0; i < AtomDataObj.noOfAtoms; i++) {
-		fscanf(fp, "%f%f%f%f", &AtomDataObj.atomCoords[0][i], &AtomDataObj.atomCoords[1][i], &AtomDataObj.atomCoords[2][i], &AtomDataObj.atomCoords[3][i]);
+		fscanf(fp, "%f%f%f", &AtomDataObj.atomCoords[0][i], &AtomDataObj.atomCoords[1][i], &AtomDataObj.atomCoords[2][i]);
 
 	}
-	//printf("%f", AtomDataObj.atomCoords[0][2]);
-}
 
-double global_sum(double partial, int myid, int nprocs) {
-	/* Implement your own global summation here */
-	double mydone = partial, hisdone;
-	int bitValue;
-	MPI_Status status;
-	for (bitValue = 1; bitValue < nprocs; bitValue *= 2) {
-		int partner = myid ^ bitValue;
-		MPI_Send(&mydone, 1, MPI_DOUBLE, partner, bitValue, MPI_COMM_WORLD);
-		MPI_Recv(&hisdone, 1, MPI_DOUBLE, partner, bitValue, MPI_COMM_WORLD, &status);
-		mydone += hisdone;
-	}
-	return mydone;
 }
 
 int main(int argc, char *argv[]) {
-	readAtoms();
-	//printf("Hello Vis");
+	double t1 = MPI_Wtime();
+	readAtoms(argv[1]);
+
 	int noOfRandomPts = 500000;
 	int nprocs;  /* Number of processors */
 	int myid;    /* My rank */
 
-	//long hits=getHits(noOfRandomPts);
-	//printf("\n%f", (float)(1.0* hits/ noOfRandomPts));
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
 	long hits = getHitsByPSequentialPoints(myid);
-	int totalHits = global_sum(hits, myid, nprocs);
-	printf("\nhits-%d", hits);
-
+	int totalHits;
+	MPI_Allreduce(&hits, &totalHits, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+	double t2 = MPI_Wtime();
 	if (myid == 0) {
-		long totalPoints = (X / CUT_OFF)*(Y / CUT_OFF)*(Z / CUT_OFF);
-		printf("\ntotalHits = %d", totalHits);
-		printf("\nTotalPoints = %f", (float)(1.0* hits / (totalPoints)));
-	}
+		//long totalPoints = (X / (2 * CUT_OFF))*(Y / (2 * CUT_OFF))*(Z / (2 * CUT_OFF));
+		printf("\ntotalHits = %d\n", totalHits);
+		printf("\ntotalPoints = %d\n", totalPoints);
+		printf("\nVolume = %f\n", (float)(1.0* totalHits / (totalPoints)));
+		printf("Total Elapsed time is %f\n", t2 - t1);
 
+	}
 	MPI_Finalize();
 
 	return 0;
-
-	/*long totalPoints = (X / CUT_OFF)*(Y / CUT_OFF)*(Z / CUT_OFF);
-	printf("\n%f", (float)(1.0* hits / (totalPoints)));*/
-	//printf("\n%f", (float)(1.0* hits/ noOfRandomPts));
-
-
 }
+
